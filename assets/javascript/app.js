410cbeee886dc446b351d6637014b339b56815e6
David Thompson <drthusker@gmail.com>
2017-12-07 18:03:50 -0700

    final 2nd

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,42 @@
 // Sports array
-  var sportsList = ['Nebraska Football', 'Scott Frost', 'Sun Devil Basketball', 'Arizona Basketball', 'Detroit Lions', 'Arizona Cardinals', 
-  'Larry Fitzgerald', 'Phoenix Suns', 'Arizona Coyotes'];
+  var sportsList = ['Nebraska Football', 'Scott Frost', 'Sun Devil Basketball', 'Arizona Basketball'];
 
 
   function displaysportsInfo(){   
     $('#sportsView').empty(); 
     var sports = $(this).attr('data-name');
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=zwbU8CcM47AjGoK1bYI5XuS7hVQMP12j&q=" + sports + "&limit=10&offset=0&rating=G&lang=en";
-
+      console.log(this);
     $.ajax({url: queryURL, method: 'GET'})
        .done(function(response) {
            var results = response.data;
-           for(var i=0; i < results.length; i++){
-              if (results[i].rating == "r" || results[i].rating == "pg-13")
-              {
+        for(var i=0; i < results.length; i++){
+          if (results[i].rating == "r" || results[i].rating == "pg-13")
+         {
+         }
+          else {
+          var rating = results[i].rating;
+          var p = $('<p>').text( "Rating: " + rating);
+          var sportsImage = $('<img>');
 
-              }
-              else {
-               var rating = results[i].rating;
-               var p = $('<p>').text( "Rating: " + rating);
-               var sportsImage = $('<img>');
+      sportsImage.attr('src', results[i].images.fixed_height_still.url);
+      sportsImage.attr('data-still', results[i].images.fixed_height_still.url);
+      sportsImage.attr('data-animate', results[i].images.fixed_height.url);
+      sportsImage.attr('data-state', 'still');
+      sportsImage.addClass('sportsImage');  
 
-               sportsImage.attr('src', results[i].images.fixed_height_still.url);
-               sportsImage.attr('data-still', results[i].images.fixed_height_still.url);
-               sportsImage.attr('data-animate', results[i].images.fixed_height.url);
-               sportsImage.attr('data-state', 'still');
-               sportsImage.addClass('sportsImage');               
-
-               $('#sportsView').append(p);
-               $('#sportsView').append(sportsImage);
+      $('#sportsView').append(p);
+      $('#sportsView').append(sportsImage);
               }
            }
 
       $('.sportsImage').on('click', function(){
-          var state = $(this).attr('data-state'); 
+        var state = $(this).attr('data-state'); 
             console.log(state);
-            if ( state == 'still'){
-                $(this).attr('src', $(this).data('animate'));
-                $(this).attr('data-state', 'animate');
-            }else{
+       if ( state == 'still'){
+          $(this).attr('src', $(this).data('animate'));
+          $(this).attr('data-state', 'animate');
+          }else{
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
             }
@@ -52,15 +50,14 @@
 
         var a = $('<button>')
         a.addClass('sports');
-        a.addClass("btn btn-dark"); 
-        a.addClass("btn btn-dark btn-lg");
         a.attr('data-name', sportsList[i]);
         a.text(sportsList[i]);
         $('#buttonsView').append(a);
     }
+
   }
   $('#addsports').on('click', function(){
-    var sports = $('#sports-input').val().trim();
+    var sports = $('#sportsInput').val().trim();
 
     sportsList.push(sports);
 
